6ab102170a909e02aaa2e0f126008e8d4cb92ae3
vicenzodev <heitor@vicenzo.dev>
2025-08-22 22:14:52 -0300

    Puxar do banco

--- src/app/condominios/page.tsx
+++ src/app/condominios/page.tsx
@@ -1,8 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import {getCondominios, ICondominio} from '../services/api-condominios';
 import Header from "../header";
 
 export default function ListaCondominios() {
+    
+    const [condominios,setCondominios] = useState<ICondominio[]>([]);
+
+    useEffect(()=>{
+        const buscarCondominios = async () => {
+            const data = await getCondominios();
+            console.log(data);
+            setCondominios(data);
+        }
+        buscarCondominios();
+    }, []);
+    
     return(
         <div>
             <Header/>
@@ -25,11 +39,25 @@ export default function ListaCondominios() {
                            </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-200 bg-white">
-                            <tr>
-                                <td className="px-4 py-3 text-sm text-gray-700" colSpan={7}>
-                                    Nenhum condominio encontrado.
-                                </td>
-                            </tr>
+                            {condominios.length === 0 ? (
+                                <tr>
+                                    <td className="px-4 py-3 text-sm text-gray-700" colSpan={7}>
+                                        Nenhum condomínio encontrado
+                                    </td>
+                                </tr>
+                            ) : (
+                                condominios.map((condominio, index) => (
+                                    <tr key={condominio.id_condominio} className="hover:bg-gray-50">
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">{String(index + 1)}</th>
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">Nome</th>
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">Endereço</th>
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">Cidade</th>
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">UF</th>
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">Tipo</th>
+                                        <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider border-2 border-gray-300">Ação</th>
+                                    </tr>
+                                ))
+                            )}
                         </tbody>
                     </table>
                 </div>
